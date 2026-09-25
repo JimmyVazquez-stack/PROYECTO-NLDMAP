aa06c5e26c91be7f63da86ff01de62f53e2327a6
Eduardo <99711045+Edwardusky@users.noreply.github.com>
2026-09-25 03:48:02 +0000

    feat(docker): simplificar la creación de usuarios de desarrollo en MongoDB y PostgreSQL

--- docker/mongo/init/10-create-developers.js
+++ docker/mongo/init/10-create-developers.js
@@ -1,9 +1,7 @@
 const required = [
   "MONGO_INITDB_DATABASE",
-  "NLDMAP_MONGO_DEV1_USER",
-  "NLDMAP_MONGO_DEV1_PASSWORD",
-  "NLDMAP_MONGO_DEV2_USER",
-  "NLDMAP_MONGO_DEV2_PASSWORD",
+  "NLDMAP_MONGO_DEV_USER",
+  "NLDMAP_MONGO_DEV_PASSWORD",
 ];
 
 for (const name of required) {
@@ -13,27 +11,10 @@ for (const name of required) {
 }
 
 const databaseName = process.env.MONGO_INITDB_DATABASE;
-const users = [
-  {
-    user: process.env.NLDMAP_MONGO_DEV1_USER,
-    pwd: process.env.NLDMAP_MONGO_DEV1_PASSWORD,
-  },
-  {
-    user: process.env.NLDMAP_MONGO_DEV2_USER,
-    pwd: process.env.NLDMAP_MONGO_DEV2_PASSWORD,
-  },
-];
-
-if (users[0].user === users[1].user) {
-  throw new Error("Los usuarios de desarrollo deben ser distintos");
-}
-
 const reportsDb = db.getSiblingDB(databaseName);
 
-for (const account of users) {
-  reportsDb.createUser({
-    user: account.user,
-    pwd: account.pwd,
-    roles: [{ role: "readWrite", db: databaseName }],
-  });
-}
+reportsDb.createUser({
+  user: process.env.NLDMAP_MONGO_DEV_USER,
+  pwd: process.env.NLDMAP_MONGO_DEV_PASSWORD,
+  roles: [{ role: "readWrite", db: databaseName }],
+});
--- docker/postgres/init/10-create-developers.sh
+++ docker/postgres/init/10-create-developers.sh
@@ -1,33 +1,23 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-: "${NLDMAP_DEV1_USER:?Falta NLDMAP_DEV1_USER}"
-: "${NLDMAP_DEV1_PASSWORD:?Falta NLDMAP_DEV1_PASSWORD}"
-: "${NLDMAP_DEV2_USER:?Falta NLDMAP_DEV2_USER}"
-: "${NLDMAP_DEV2_PASSWORD:?Falta NLDMAP_DEV2_PASSWORD}"
+: "${NLDMAP_DEV_USER:?Falta NLDMAP_DEV_USER}"
+: "${NLDMAP_DEV_PASSWORD:?Falta NLDMAP_DEV_PASSWORD}"
 
 psql \
-    --username "$POSTGRES_USER" \
-    --dbname "$POSTGRES_DB" \
-    --set=dev1_user="$NLDMAP_DEV1_USER" \
-    --set=dev1_password="$NLDMAP_DEV1_PASSWORD" \
-    --set=dev2_user="$NLDMAP_DEV2_USER" \
-    --set=dev2_password="$NLDMAP_DEV2_PASSWORD" \
-    --set=db_name="$POSTGRES_DB" \
-    --set=ON_ERROR_STOP=1 <<'SQL'
+  --username "$POSTGRES_USER" \
+  --dbname "$POSTGRES_DB" \
+  --set=dev_user="$NLDMAP_DEV_USER" \
+  --set=dev_password="$NLDMAP_DEV_PASSWORD" \
+  --set=db_name="$POSTGRES_DB" \
+  --set=ON_ERROR_STOP=1 <<'SQL'
 CREATE ROLE nldmap_developers NOLOGIN;
 
-CREATE ROLE :"dev1_user"
-    LOGIN PASSWORD :'dev1_password'
+CREATE ROLE :"dev_user"
+    LOGIN PASSWORD :'dev_password'
     NOSUPERUSER NOCREATEDB NOCREATEROLE;
 
-CREATE ROLE :"dev2_user"
-    LOGIN PASSWORD :'dev2_password'
-    NOSUPERUSER NOCREATEDB NOCREATEROLE;
-
-GRANT nldmap_developers TO :"dev1_user";
-GRANT nldmap_developers TO :"dev2_user";
-
+GRANT nldmap_developers TO :"dev_user";
 GRANT CONNECT ON DATABASE :"db_name" TO nldmap_developers;
 GRANT USAGE, CREATE ON SCHEMA public TO nldmap_developers;
 
